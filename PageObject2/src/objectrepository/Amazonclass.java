d26dd4eb300989d3b1894b48c8f5f4b2689e6355
karthikpobbathi <35491482+karthikpobbathi@users.noreply.github.com>
2019-12-18 10:59:41 +0530

    Add files via upload

---
+++ PageObject2/src/objectrepository/Amazonclass.java
@@ -0,0 +1,12 @@
+package objectrepository;
+
+import org.openqa.selenium.WebDriver;
+
+public class Amazonclass {
+	WebDriver driver;
+	public Amazonclass(WebDriver driver) {
+		this.driver=driver;
+		
+	}
+	
+}
